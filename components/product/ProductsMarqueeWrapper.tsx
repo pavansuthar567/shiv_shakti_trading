1a7b7d3690ac4a3dec407118c341272f21109655
Pavan Suthar <53314417+pavansuthar567@users.noreply.github.com>
2024-08-03 20:54:01 +0530

    test deploy

--- components/product/ProductsMarqueeWrapper.tsx
+++ components/product/ProductsMarqueeWrapper.tsx
@@ -6,7 +6,7 @@ export default async function ProductsMarqueeWrapper() {
   return (
     <section className="py-4 md:py-10">
       <h2 className="p-2 pt-0 text-lg font-medium md:p-4 md:pt-0 md:text-xl">
-        Our Most Popular Products
+        Our Most Popular Products1
       </h2>
       <ProductsMarquee products={products} />
     </section>
